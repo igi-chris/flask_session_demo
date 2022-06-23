4016334c57f0240366f1015bf94752fd662fd0d8
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-06-23 18:47:27 +0100

    using models for predictions

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import imp
 import os
-from flask import Flask, render_template, session, request, jsonify
+from random import randint
+from flask import Flask, render_template, session, request
 from flask_session import Session
 import numpy as np
 import pandas as pd
-from pyparsing import col
 import redis
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 # Check Configuration section for more details
@@ -37,20 +37,38 @@ def overwrite_session_data():
             session[param_name] = val
     return index()
 
-@app.route('/create_df/')
-def create_df():
+@app.route('/create_df_and_model/')
+def create_df_and_model():
     ncols = request.args.get("df_n_cols", 5, type=int)
     nrows = request.args.get("df_n_rows", 10, type=int)
     cols = [chr(i+65) for i in range(ncols)]
     
-    ndfs = sum(1 for val in session.values() if isinstance(val, pd.DataFrame))
     df = pd.DataFrame(np.random.randint(0,100,size=(nrows, ncols)), columns=cols)
-    session[f'df_{ndfs}'] = df
+    if 'dfs' not in session:
+        session['dfs'] = []
+    session[f'dfs'].append(df)
+
+    model = LinearRegression()
+    result_func = lambda row: sum([(v * (i%3)) + i for i,v in enumerate(row)])
+    results = list(map(result_func, [r.values for _, r in df.iterrows()]))
+    model.fit(X=df, y=results)    
+    if 'models' not in session:
+        session['models'] = []
+    session[f'models'].append(model)
+    use_models()
+
     return index()
 
-@app.route('/get/', methods=['GET'])
-def get():
-    return jsonify(session)
+#@app.route('/predict/')
+def use_models():
+    # the purpse is just to demonstrate that the objects we get back work, actual numbers are throw away
+    # regen all intentially - want the need to get dfs and cache from session and use
+    predictions = []
+    df: pd.DataFrame
+    model: LinearRegression
+    for df, model in zip(session["dfs"], session["models"]):
+        predictions.append(model.predict(df))
+    session["predictions"] = predictions
 
 @app.route('/reset/')
 def reset():
