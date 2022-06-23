3d1c6dc314aedd04232d8d8e898c326eeeba1ac5
igi-chris <62021095+igi-chris@users.noreply.github.com>
2022-06-23 11:03:03 +0100

    display in main screen

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, session, request, jsonify
 from flask_session import Session
 import os
 import redis
+import json
 
 app = Flask(__name__)
 # Check Configuration section for more details
-#igiml.redis.cache.windows.net:6380,password=7pfsx6oSloLNoWnVhjZIl8MSgMIhjYV45AzCaLPA0dw=,ssl=True,abortConnect=False
 redis_pwd = os.getenv('IGI_ML_REDIS_PWD')
 SESSION_TYPE = 'redis'
 r = redis.Redis(host="igiml.redis.cache.windows.net", port=6380, 
@@ -14,6 +14,19 @@ SESSION_REDIS = r
 app.config.from_object(__name__)
 Session(app)
 
+@app.route("/", methods=['GET'])
+def index() -> str:
+    raw_data = get().data.decode('utf-8')
+    session_state = json.loads(raw_data)
+    return render_template('index.html', data=session_state)
+
+@app.route('/get/', methods=['GET'])
+def get():
+    return jsonify(
+        key1=session.get("key1", []),
+        key2=session.get("key2", []),
+        key3=session.get("key3", []))
+        
 @app.route('/set/')
 def set():
     for param_name, val in request.args.items():
@@ -23,19 +36,8 @@ def set():
             session[param_name].append(val)
     return index()
 
-@app.route('/get/')
-def get():
-    return jsonify(
-        key1=session.get("key1", []),
-        key2=session.get("key2", []),
-        key3=session.get("key3", []))
-
-@app.route("/", methods=['GET'])
-def index() -> str:
-    return render_template('index.html')
-
-@app.route('/reset/')
-def reset():
-    for param_name in request.args.keys():
-        session[param_name] = []
-    return get()
+# @app.route('/reset/')
+# def reset():
+#     for param_name in request.args.keys():
+#         session[param_name] = []
+#     return get()
